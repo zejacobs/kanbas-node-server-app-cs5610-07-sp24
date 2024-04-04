f78d463e78e7ca7a00bc704ebee613d7c6a95500
zejacobs <jacobs.za@northeastern.edu>
2024-04-04 02:26:58 -0400

    finish a5

--- App.js
+++ App.js
@@ -5,11 +5,13 @@ import Lab5 from "./Lab5.js";
 import cors from "cors";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
+import AssignmentRoutes from "./Kanbas/assignments/routes.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+AssignmentRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
 Lab5(app);
---
+++ Kanbas/assignments/routes.js
@@ -0,0 +1,36 @@
+import db from "../Database/index.js";
+
+export default function AssignmentRoutes(app) {
+  app.delete("/api/assignments/:aid", (req, res) => {
+    const { aid } = req.params;
+    db.assignments = db.assignments.filter((a) => a._id !== aid);
+    res.sendStatus(200);
+  });
+
+  app.put("/api/assignments/:aid", (req, res) => {
+    const { aid } = req.params;
+    const assignmentIndex = db.assignments.findIndex((a) => a._id === aid);
+    db.assignments[assignmentIndex] = {
+      ...db.assignments[assignmentIndex],
+      ...req.body,
+    };
+    res.sendStatus(204);
+  });
+
+  app.post("/api/assignments/:cid", (req, res) => {
+    const { cid } = req.params;
+    const newAssignment = {
+      ...req.body,
+      course: cid,
+      _id: new Date().getTime().toString(),
+    };
+    db.assignments.push(newAssignment);
+    res.send(newAssignment);
+  });
+
+  app.get("/api/assignments/:cid", (req, res) => {
+    const { cid } = req.params;
+    const assignments = db.assignments.filter((a) => a.course === cid);
+    res.send(assignments);
+  });
+}
