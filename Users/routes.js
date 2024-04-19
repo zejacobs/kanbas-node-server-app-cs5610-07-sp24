23cf98b04c20f0b66df3617915308e3098debd2e
zejacobs <jacobs.za@northeastern.edu>
2024-04-18 22:06:52 -0400

    fix a6

--- Users/routes.js
+++ Users/routes.js
@@ -36,7 +36,6 @@ export default function UserRoutes(app) {
   const updateUser = async (req, res) => {
     const { userId } = req.params;
     const status = await dao.updateUser(userId, req.body);
-    currentUser = await dao.findUserById(userId);
     res.json(status);
   };
   app.put("/api/users/:userId", updateUser);
